e2b33322dfd5555586d738855218c889fb44d677
Gustavo Reis <gustavoazreis@gmail.com>
2025-03-09 14:38:53 -0300

    ajustando pipeline

--- dags/api_to_postgres.py
+++ dags/api_to_postgres.py
@@ -22,10 +22,15 @@ def api_pokemon_postgres():
     def task_add_pokemon_to_db(pokemon_data):
         return add_pokemon_to_db(pokemon_data)
 
+    @task
+    def print_de_sucesso(response):
+        print(response)
+    
     t1 = task_gerar_numero_aleatorio()
     t2 = task_fetch_pokemon_data(t1)
     t3 = task_add_pokemon_to_db(t2)
+    t4 = print_de_sucesso(t3)
 
-    t1 >> t2 >> t3
+    t1 >> t2 >> t3 >> t4
 
-capturar_pokemon()
+api_postgres()
